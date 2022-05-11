29ff77e2d10b103b3c13bb9493c1dec996891745
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-05-11 14:55:29 +0200

    button und bookmark müssen überarbeitet werden

---
+++ script.js
@@ -0,0 +1,16 @@
+function booking() {
+  alert("ICH HABE DIE SCHNAUZE VOLL !!!!!!!!!");
+}
+// function answerbutton() {
+//   alert("und hiervon auch !!!!!!!!!!");
+// }
+
+let showHide = document.querySelector(".bigansw");
+showHide.addEventListener("click", () => {
+  alert("Auch das funktionert nicht,wetten ?");
+});
+
+// var klick = document.querySelector(".bigansw");
+// klick.addEventListener("click", () => {
+//   alert("???");
+// });
