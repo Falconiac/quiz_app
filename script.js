529d8f9c24f4dc250f44fbee7c13022c56f2b860
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-05-12 20:17:29 +0200

    button and bookmark works

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let showHide = document.querySelector(".bigansw");
-showHide.addEventListener("click", () => {
-  alert("Auch das funktionert nicht,wetten ?");
+let bookmark = document.querySelectorAll("[data-js=trybackground]");
+let answerBtn = document.querySelectorAll("[data-js=answBtn]");
+let answer = document.querySelectorAll(".unvisible");
+
+function setVisible(item) {
+  if (item.style.visibility === "hidden") {
+    item.style.visibility = "visible";
+  } else {
+    item.style.visibility = "hidden";
+  }
+}
+// Funktionen
+
+function visible() {
+  visibility ? visible : hidden;
+}
+
+bookmark.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.classList.toggle("--bookmarked");
+  });
 });
 
-// var klick = document.querySelector(".bigansw");
-// klick.addEventListener("click", () => {
-//   alert("???");
-// });
+answerBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".unvisible").classList.toggle("visible");
+  });
+});
