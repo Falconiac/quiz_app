f6859b26a31995cf66c054443bb65d7f3dfbed7c
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-05-13 15:43:48 +0200

    button works, bookmark too

---
+++ js_files/bookmark.js
@@ -0,0 +1,9 @@
+function toggleclass(element) {
+  element.classList.toggle("--bookmarked");
+}
+
+function show_hide_click(element) {
+  element.classList.toggle("visible");
+}
+
+export { toggleclass, show_hide_click };
--- script.js
+++ script.js
@@ -1,28 +1,59 @@
-let bookmark = document.querySelectorAll("[data-js=trybackground]");
-let answerBtn = document.querySelectorAll("[data-js=answBtn]");
-let answer = document.querySelectorAll(".unvisible");
+import { toggleclass, show_hide_click } from "./js_files/bookmark.js";
+// Bookmark-Block funktionert !!!
+// let bookmarks = document.querySelectorAll("[data-js=trybackground]");
 
-function setVisible(item) {
-  if (item.style.visibility === "hidden") {
-    item.style.visibility = "visible";
-  } else {
-    item.style.visibility = "hidden";
-  }
-}
-// Funktionen
+// bookmarks.forEach((element) => {
+//   bookmarker(element);
+// });
+// Bookmark-Block funktionert !!!
 
-function visible() {
-  visibility ? visible : hidden;
-}
+let cards = document.querySelectorAll(".questcard");
+cards.forEach((card) => {
+  let bookmark = card.querySelector("[data-js=trybackground]");
+  let button = card.querySelector("[data-js=answBtn]");
+  let answer = card.querySelector(".unvisible");
 
-bookmark.forEach((element) => {
-  element.addEventListener("click", () => {
-    element.classList.toggle("--bookmarked");
+  bookmark.addEventListener("click", () => {
+    toggleclass(bookmark);
+  });
+
+  button.addEventListener("click", () => {
+    show_hide_click(answer);
+
+    if (button.innerHTML === "Show Answer") {
+      button.innerHTML = "Hide Answer";
+      button.classList.remove("background_green");
+      button.classList.add("background_red");
+    } else {
+      button.innerHTML = "Show Answer";
+      button.classList.remove("background_red");
+      button.classList.add("background_green");
+    }
   });
 });
 
-answerBtn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector(".unvisible").classList.toggle("visible");
-  });
-});
+// Baugtastic-Block
+
+// let bookmarks = document.querySelectorAll("[data-js=trybackground]");
+// let answerBtns = document.querySelectorAll("[data-js=answBtn]");
+// let answers = document.querySelectorAll(".unvisible");
+
+// // Funktionen
+// function show_hide(btn) {
+//   btn.addEventListener("click", () => {
+//     answers.forEach((answer) => {
+//       answer.classList.toggle("visible");
+//       console.log(answer);
+//     });
+//   });
+// }
+
+// // Funktionen
+
+// bookmarks.forEach((element) => {
+//   bookmarker(element);
+// });
+
+// answerBtns.forEach((btn) => {
+//   show_hide(btn);
+// });
